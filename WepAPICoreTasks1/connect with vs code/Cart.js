1e462447591d03bbf329d9f0880b934e852772d4
hmoodeh98 <hmoodeh9820@gmail.com>
2024-08-29 17:34:54 +0300

    Edit&Delete

--- WepAPICoreTasks1/connect with vs code/Cart.js
+++ WepAPICoreTasks1/connect with vs code/Cart.js
@@ -15,8 +15,9 @@ async function getCartItems() {
         <tr>
             <td>${element.cartId}</td>
             <td>${element.product.productName}</td>
-            <td><input type="number" name="quantity" id="quantity" value="${element.quantity}" ></input></td>
-            <td><a class="btn btn-primary"  onclick="store(${element.cartItemId})">Edit</a></td>
+            <td><input type="number" name="quantity" id="quantity" value="" placeholder="${element.quantity}" ></input></td>
+            <td><a class="btn btn-primary"  onclick="edit(${element.cartItemId})">Edit</a></td>
+            <td><a class="btn btn-primary"  onclick="delet(${element.cartItemId})">Delete</a></td>
         </tr>
         <!-- Add more rows as needed -->
                     
@@ -25,9 +26,54 @@ async function getCartItems() {
     })
 
 }
-function store(id){
-    localStorage.setItem("cartItemId",id);
+
+async function edit(id){
+    debugger;
+    event.preventDefault();
+
+    const url = `https://localhost:44367/api/CartItem/UpdateItem/${id}`;
+    let quantity = document.getElementById("quantity");
+    let container = document.getElementById("container");
+    var request = {
+        quantity: quantity.value,
+    }
+
+    let response = await fetch(url, {
+        method: 'PUT',
+        body : JSON.stringify(request),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    alert("Item updated successfully");
+
 }
+
+async function delet(id){
+    debugger;
+    event.preventDefault();
+    const url = `https://localhost:44367/api/CartItem/DeleteItem/${id}`;
+    let container = document.getElementById("container");
+    var request = {
+        cartItemId: id,
+    }
+    let response = await fetch(url, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+        // body : JSON.stringify(request),
+        
+        
+
+        
+    });
+    alert("Item deleted successfully");
+}
+
+// function store(id){
+//     localStorage.setItem("cartItemId",id);
+// }
 
 function reset(){
     localStorage.clear();
